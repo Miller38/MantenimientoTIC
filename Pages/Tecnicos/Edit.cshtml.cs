900038442693e3f8ae868d9529fbb6f3d6d4a8c4
Miller38 <millergutierrez38@gmail.com>
2023-02-12 12:22:49 -0600

    first commit

---
+++ Pages/Tecnicos/Edit.cshtml.cs
@@ -0,0 +1,80 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Mantenimiento.Dominio;
+using Mantenimiento.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Mantenimiento.Pages_Tecnicos
+{
+    [Authorize]
+    public class EditModel : PageModel
+    {
+        private readonly Mantenimiento.Persistencia.MantenimientoContext _context;
+
+        public EditModel(Mantenimiento.Persistencia.MantenimientoContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Tecnico Tecnico { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Tecnico = await _context.Tecnico.FirstOrDefaultAsync(m => m.ID == id);
+
+            if (Tecnico == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Tecnico).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TecnicoExists(Tecnico.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool TecnicoExists(int id)
+        {
+            return _context.Tecnico.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ Pages/Vehiculos/Details.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Mantenimiento.Dominio;
+using Mantenimiento.Persistencia;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace Mantenimiento.Pages_Vehiculos
+{
+    [Authorize]
+    public class DetailsModel : PageModel
+    {
+        private readonly Mantenimiento.Persistencia.MantenimientoContext _context;
+
+        public DetailsModel(Mantenimiento.Persistencia.MantenimientoContext context)
+        {
+            _context = context;
+        }
+
+        public Vehiculo Vehiculo { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Vehiculo = await _context.Vehiculo
+                .Include(v => v.Servicios)
+                .ThenInclude(s => s.Tecnico)
+                .FirstOrDefaultAsync(m => m.VehiculoID == id);
+
+            if (Vehiculo == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
